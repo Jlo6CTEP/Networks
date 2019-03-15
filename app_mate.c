210305e07560519858537244536d6cdbedc812c8
Jlo6CTEP <35602222+Jlo6CTEP@users.noreply.github.com>
2019-03-15 22:04:02 +0300

    aaalmost there

--- app_mate.c
+++ app_mate.c
@@ -330,6 +330,7 @@ void* udp_client(void *nothing) {
         while(is_error >= 0) {
             pthread_mutex_lock(&lock);
             network_node *nn = array_list_get(node_list, iter, &is_error);
+            nn->node_address.sin_port = UDP_SERVER_PORT;
             iter = array_list_next(node_list, iter, &is_error);
             pthread_mutex_unlock(&lock);
             printf("pingin node with name %s\n", nn->name);
